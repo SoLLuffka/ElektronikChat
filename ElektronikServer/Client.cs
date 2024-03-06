ae2ea0b7bcf8ed4a222b6aad13872c0ab41831f6
HyxerX <kamil2005kr@gmail.com>
2024-03-07 00:27:10 +0100

    chat building 1

---
+++ ElektronikServer/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElektronikServer
+{
+    internal class Client
+    {
+        public string Username { get; set; }
+        public Guid UID { get; set; }
+        public TcpClient ClientScoket { get; set; }
+        public Client(TcpClient client)
+        {
+            ClientScoket = client;
+            UID = Guid.NewGuid();
+
+            Console.WriteLine($"[{DateTime.Now}]:{Username} has connected");
+        }
+    }
+}
---
+++ ElektronikServer/program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElektronikServer
+{
+   class program
+    {
+        static List<Client> _users;
+        static TcpListener _listener;
+        static void Main(string[] args)
+        {
+            _users = new List<Client>();
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 22000);
+            _listener.Start();
+
+            while (true)
+            {
+                var client = new Client(_listener.AcceptTcpClient());
+                _users.Add(client);
+
+                /*Broadcast the connection to everyone on server*/
+            }
+        }
+    }
+}
