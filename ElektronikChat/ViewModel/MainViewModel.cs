621ec3c66b2ef463b721d8a733af616e6bdf3534
HyxerX <kamil2005kr@gmail.com>
2024-03-10 17:59:32 +0100

    Chat core to repair polish characters

--- ElektronikChat/ViewModel/MainViewModel.cs
+++ ElektronikChat/ViewModel/MainViewModel.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace ElektronikChat.ViewModel
 {
--- ElektronikServer/Net/IO/PacketReader.cs
+++ ElektronikServer/Net/IO/PacketReader.cs
@@ -22,7 +22,7 @@ namespace ElektronikServer.Net.IO
             msgBffer = new byte[length];
             _ns.Read(msgBffer, 0, length);
 
-            var msg = Encoding.ASCII.GetString(msgBffer);
+            var msg = Encoding.UTF8.GetString(msgBffer);
             return msg;
         }
     }
